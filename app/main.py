8f134936cc163027b6019c52693b2166f7dc4c97
Sahil Malakar <sahilmalakar150@gmail.com>
2026-04-24 01:30:31 +0530

    added cors logic

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, status
 from app import model
 from app.db import engine
 from app.routers import posts , users , auth ,votes
+from fastapi.middleware.cors import CORSMiddleware
 
-model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Social media app",
@@ -11,6 +11,22 @@ app = FastAPI(
     version="1.0.0",
     docs_url="/docs",
     redoc_url="/redoc"
+)
+
+
+origins = [
+    "http://localhost:3000",   # React
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",   # Vite
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # or ["*"] for testing
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, DELETE etc.
+    allow_headers=["*"],            # Authorization, Content-Type etc.
 )
 
 app.include_router(posts.router)
